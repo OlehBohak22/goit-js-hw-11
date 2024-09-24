703240f879870326ef74722b8873425bdc46d2bd
Bohak Oleh <eddrfgthyjn@gmail.com>
2024-09-24 20:22:01 +0300

    .

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -6,10 +6,10 @@ export function renderGallery(images) {
     <a href="${image.largeImageURL}" class="gallery-item">
       <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
       <div class="info">
-        <p><b>Likes</b>: ${image.likes}</p>
-        <p><b>Views</b>: ${image.views}</p>
-        <p><b>Comments</b>: ${image.comments}</p>
-        <p><b>Downloads</b>: ${image.downloads}</p>
+        <p><b>Likes</b> ${image.likes}</p>
+        <p><b>Views</b> ${image.views}</p>
+        <p><b>Comments</b> ${image.comments}</p>
+        <p><b>Downloads</b> ${image.downloads}</p>
       </div>
     </a>
   `
